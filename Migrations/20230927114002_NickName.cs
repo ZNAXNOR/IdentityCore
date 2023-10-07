aa23dcf6697d61b3f0d70d06920477482d498c43
Omkar Dalvi <121810601+ZNAXNOR@users.noreply.github.com>
2023-10-07 22:37:51 +0530

    Migrations

---
+++ Migrations/20230927114002_NickName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class NickName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NickName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NickName",
+                table: "AspNetUsers");
+        }
+    }
+}
