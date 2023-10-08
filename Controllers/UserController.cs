7d48b9b5cda88c189ab848ce6f51d5a1a755ad26
Omkar Dalvi <121810601+ZNAXNOR@users.noreply.github.com>
2023-10-08 12:53:35 +0530

    User Edit & User Delete

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using IdentityCore.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace IdentityCore.Controllers
 {
@@ -16,6 +17,8 @@ namespace IdentityCore.Controllers
             _userManager = userManager;
         }
 
+
+        // Index
         public IActionResult Index()
         {
             var userList = _db.AppUser.ToList();
@@ -37,23 +40,78 @@ namespace IdentityCore.Controllers
             return View(userList);
         }
 
+
+        // Edit
+        [HttpGet]
+        public IActionResult Edit(string userId)
+        {
+            var user = _db.AppUser.FirstOrDefault(u => u.Id == userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            var role = userRole.FirstOrDefault(u => u.UserId == user.Id);
+            if (role != null)
+            {
+                user.RoleId = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
+            }
+            user.RoleList = _db.Roles.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+            return View(user);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(AppUser user)
         {
-            var userDbValue = _db.AppUser.FirstOrDefault(u => u.Id == user.Id);
-            if (userDbValue == null)
+            if (ModelState.IsValid)
+            {
+                var userDbValue = _db.AppUser.FirstOrDefault(u => u.Id == user.Id);
+                if (userDbValue == null)
+                {
+                    return NotFound();
+                }
+
+                var userRole = _db.UserRoles.FirstOrDefault(u => u.UserId == userDbValue.Id);
+                if (userRole != null)
+                {
+                    var previousRoleName = _db.Roles.Where(u => u.Id == userRole.RoleId).Select(e => e.Name).FirstOrDefault();
+                    await _userManager.RemoveFromRoleAsync(userDbValue, previousRoleName);
+
+                }
+
+                await _userManager.AddToRoleAsync(userDbValue, _db.Roles.FirstOrDefault(u => u.Id == user.RoleId).Name);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            user.RoleList = _db.Roles.Select(u => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+
+            return View(user);
+        }
+
+
+        // Delete
+        [HttpPost]
+        public IActionResult Delete(string userId)
+        {
+            var user = _db.AppUser.FirstOrDefault(u => u.Id == userId);
+            if (user == null)
             {
                 return NotFound();
             }
-            var userRole = _db.UserRoles.FirstOrDefault(u => u.UserId == userDbValue.Id);
-            if (userRole != null)
-            {
-                var previousRoleName = _db.Roles.Where(u => u.Id == userRole.RoleId).Select(e => e.Name).FirstOrDefault();
-                await _userManager.RemoveFromRoleAsync(userDbValue, previousRoleName);
-            }
-            await _userManager.AddToRoleAsync(userDbValue, _db.Roles.FirstOrDefault(u => u.Id == user.RoleId).Name);
+            _db.AppUser.Remove(user);
             _db.SaveChanges();
+
             return RedirectToAction(nameof(Index));
         }
     }
